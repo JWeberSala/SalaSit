87d8e5f9fbf29f29eefa7ed540ba11488d724ea8
Matias Damian Duque de Arce <duque_damian@hotmail.com>
2022-10-26 08:49:27 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,69 @@
-function validacion_al_enviar_datos() {
+const btn_guardar = document.querySelector('#btn-guardar');
+const nombre = document.querySelector('#Nombre');
+const apellido = document.querySelector('#Apellido');
+const email = document.querySelector('#Email');
+const consulta = document.querySelector('#Consulta');
 
-    if (document.fcontacto.nombre.value.length == 0) {
-        alert("Falta escribir el nombre")
-        document.fcontacto.nombre.focus()
-        return 0;
+
+let validar = () => {
+    let inputs_requeridos = document.querySelectorAll('#frm-registro [required]');
+    let error = false;
+
+    for (let i = 0; i < inputs_requeridos.length; i++) {
+        if (inputs_requeridos[i].value == '') {
+            inputs_requeridos[i].classList.add('input-error');
+            error = true;
+        } else {
+            inputs_requeridos[i].classList.remove('input-error');
+        }
     }
 
+    return error;
+};
 
-    if (document.fcontacto.apellido.value.length == 0) {
-        alert("Falta escribir el apellido")
-        document.fcontacto.apellido.focus()
-        return 0;
+let limpiar = () => {
+    nombre.value = "";
+    apellido.value = "";
+    email.value = "";
+    consulta.value = "";
+};
+
+let obtener_datos = () => {
+    let error = validar();
+
+    if (error) {
+        Swal.fire({
+            'title': 'Sus datos no se pudieron enviar',
+            'text': 'Por favor revise los campos resaltados',
+            'icon': 'warning'
+        });
+    } else {
+        console.log(nombre.value);
+        console.log(apellido.value);
+        console.log(email.value);
+        console.log(consulta.value);
+        Swal.fire({
+            'title': 'Tu mensaje fue enviado!',
+            'text': 'Gracias por contactarte, nos comunicaremos con vos lo mas pronto posible.',
+            'icon': 'success'
+        }).then(() => {
+            limpiar();
+        });
     }
+};
 
-    if (document.fcontacto.email.value.length == 0) {
-        alert("Falta escribir el email")
-        document.fcontacto.email.focus()
-        return 0;
-    }     
+btn_guardar.addEventListener('click', obtener_datos);
 
+const $form = document.querySelector('#frm-registro')
+$form.addEventListener('submit', handleSubmit)
 
-     if (document.fcontacto.consulta.value.length == 0) {
-        alert("Falta escribir el mensaje")
-        document.fcontacto.consulta.focus()
-        return 0;
-    }
+async function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body:form,
+    headers:{
+      'Accept': 'application/json'      }
+  })
 }
